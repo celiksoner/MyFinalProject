f31e607a1c0fac00faae46effed21017384cb7a5
Soner Çelik <snrclk94@outlook.com>
2021-07-31 12:29:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var product in productManager.GetProductDetails().Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
+            } 
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+    }
+}
---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,61 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+
+        public void Add(TEntity entity)
+        {
+            // IDispossable Pattern İmplementation Of C#
+            // Using arasına yazılan kod GarbageCollector tarafından kullanıldıktan sonra hemen bellekten silinir.
+            using TContext context = new TContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            // IDispossable Pattern İmplementation Of C#
+            using TContext context = new TContext();
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            // IDispossable Pattern İmpementation Of C#
+            using TContext context = new TContext();
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using TContext context = new TContext();
+            return context.Set<TEntity>().SingleOrDefault(filter);
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using TContext context = new TContext();
+            // Ternary Operatörü kullanıyoruz. Filtre null ise listeyi olduğu gibi getir eğer filtre var ise filtrelenen veriyi linq sorgusu ile getir. 
+            return filter == null
+            ? context.Set<TEntity>().ToList()
+            : context.Set<TEntity>().Where(filter).ToList();
+        }
+
+
+    }
+}
